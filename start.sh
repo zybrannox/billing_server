1f3583b78e7750b89a3e638b15591704a7bd877e
zybrannox <zybrannox@gmail.com>
2025-12-02 01:14:06 +0530

    Folders

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ source venv/bin/activate
 # Run the production server
 exec uvicorn app.main:app \
     --host 0.0.0.0 \
-    --port 8000 \
+    --port 8080 \
     --workers 4 \
     --log-level info
